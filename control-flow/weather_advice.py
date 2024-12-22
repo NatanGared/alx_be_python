4c6049bc16c6d8093206fa5c78fc9fcb0d6785c8
NatanGared <188752465+NatanGared@users.noreply.github.com>
2024-12-22 20:09:31 +0300

    this is the general change

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
 guess = input("What's the weather like today? (sunny/rainy/cold): ")
 
 if guess.lower() == "sunny":
-    print("Wear a t-shirt and sunglasses")
+    print("Wear a t-shirt and sunglasses.")
 elif guess.lower() == "rainy":
-    print("Don't to forget your umbrella and a raincoat.")
+    print("Don't forget your umbrella and a raincoat.")
 elif guess.lower() == "cold":
-    print("Make sure to wear a warm coat and scarf.")
+    print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
