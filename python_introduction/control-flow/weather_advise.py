3d1909e5d96723a58a9935fb7697ddb55b9a35d5
NatanGared <188752465+NatanGared@users.noreply.github.com>
2024-12-22 19:44:15 +0300

    python file has been prepared

---
+++ python_introduction/control-flow/weather_advise.py
@@ -0,0 +1,10 @@
+guess = input("What's the weather like today? (sunny/rainy/cold): ")
+
+if guess.lower() == "sunny":
+    print("Wear a t-shirt and sunglasses")
+elif guess.lower() == "rainy":
+    print("Don't to forget your umbrella and a raincoat.")
+elif guess.lower() == "cold":
+    print("Make sure to wear a warm coat and scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
