380ca99fe5ff1bf3c2c2525e351826fd57a611a4
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-01-08 20:42:50 +0300

    Kindly add

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,15 @@
 def perform_operation(num1, num2, operation):
     if operation == "add":
-        solution = float(num1) + float(num2)
+        solution = num1 + num2
     elif operation == "subtract":
-        solution = float(num1) - float(num2)
+        solution = num1 - num2
     elif operation == "multiply":
-        solution = float(num1) * float(num2)
+        solution = num1 * num2
     elif operation == "divide":
         if num2 == 0:
             print("That is an invalid operation")
         else:
-            solution = float(num1) / float(num2)
+            solution = num1 / num2
     else:
         print("you have inserted the wrong kind of data or argument! Kindly review what you have done!")
 
