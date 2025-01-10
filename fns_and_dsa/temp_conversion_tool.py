b9e9d7b0b0be723a9447bd1b4d3563744cd944b4
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-01-10 14:02:43 +0300

    Kindly add

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,31 +1,25 @@
-X = 0
-FAHRENHEIT_TO_CELSIUS_FACTOR = ((X +32) *(5/9))
-CELSIUS_TO_FAHRENHEIT_FACTOR = (((9/5)* X)+32)
+FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
+CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
 
 def convert_to_celsius(fahrenheit):
     global FAHRENHEIT_TO_CELSIUS_FACTOR
-    global X
 
-    X = fahrenheit
-    FAHRENHEIT_TO_CELSIUS_FACTOR = ((X +32) *(5/9))
-    return FAHRENHEIT_TO_CELSIUS_FACTOR
+    X = (fahrenheit + 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return X
 
 def convert_to_fahrenheit(celsius):
     global CELSIUS_TO_FAHRENHEIT_FACTOR
-    global X
 
-    X = celsius
-    CELSIUS_TO_FAHRENHEIT_FACTOR = (((9/5)* X)+32)
-    return CELSIUS_TO_FAHRENHEIT_FACTOR
+    X = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return X
 
 A = int(input("Enter the temperature to convert: "))
 B = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
 
-print(convert_to_celsius(-40))
-if B == "C":
-    print(f"{A}°F is {convert_to_fahrenheit(A)}°C")
-elif B == "F":
-    print(f"{A}°C is {convert_to_celsius(A)}°F")
+if B == "F":
+    print(f"{A}°F is {convert_to_celsius(A)}°C")
+elif B == "C":
+    print(f"{A}°C is {convert_to_fahrenheit(A)}°F")
 else:
     print("“Invalid temperature. Please enter a numeric value.”")
 
