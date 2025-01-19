1c7b5e6516f11d0476810ba88559a7c6947c18dd
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-01-19 11:56:07 +0300

    trial test

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self,account_balance,initial_balance=None):
+        self.account_balance = account_balance
+        self.initial_balance = initial_balance
+
+    def deposit(self,amount):
+        return amount + self.account_balance
+
+
+    def withdraw(self,amount):
+        if self.account_balance < amount:
+            return f"There is insufficient balance. {self.account_balance}"
+        else:
+            return self.account_balance - amount
+
+
+    def display_balance(self):
+        print(f"Your current balance is {self.account_balance}")
