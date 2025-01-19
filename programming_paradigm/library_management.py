96edeb8b70157243e9ca4fb237db3774d0eb3a90
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-01-19 22:21:36 +0300

    Items were edited

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,18 @@
+class Book:
+    def __init__(self, title,author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+class Library:
+    def __init__(self):
+        self._books = ()
+    def add_book():
+        pass
+    def check_out_book(title):
+        pass
+    def return_book(title):
+        pass
+    def list_available_books():
+        pass
+
