cddd7c26b399bdfa2b3bc86a7b6857897b272b6c
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-01-19 22:32:34 +0300

    Items were edited

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -4,15 +4,49 @@ class Book:
         self.author = author
         self._is_checked_out = False
 
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        return not self._is_checked_out
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
 class Library:
     def __init__(self):
-        self._books = ()
-    def add_book():
-        pass
-    def check_out_book(title):
-        pass
-    def return_book(title):
-        pass
-    def list_available_books():
-        pass
+        self._books = []
+    
+    def add_book(self, book):
+        self._books.append(book)
+    
+    def check_out_book(self,title):
+        self.title = title
+
+        for book in self._books:
+            if book.title == title:
+                return book.check_out()
+        return False
+    
+    def return_book(self,title):
+        self.title = title
+
+        for book in self._books:
+            if book.title == title:
+                return book.return_book()
+        return False
+    
+    def list_available_books(self):
+        available_books = [book for book in self._books if book.is_available()]
+        for book in available_books:
+            print(book)
 
