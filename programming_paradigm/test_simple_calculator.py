a4b8effa18bea0e3b6a4e50e2824ed13e480c1a4
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-01-19 21:47:54 +0300

    Items were edited

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,24 +1,24 @@
 import unittest
 import simple_calculator
 
-class SimpleCalculator(unittest.TestCase):
+class Calculator(unittest.TestCase):
     def __init__(self):
         pass
 
     def test_subtract(self):
-       result = simple_calculator.subtract(6,7)
+       result = SimpleCalculator.subtract(6,7)
        self.assertEqual(result, -1)
 
     def test_add(self):
-       result = simple_calculator.add(6,7)
+       result = SimpleCalculator.add(6,7)
        self.assertEqual(result,13)
 
     def test_multiply(self):
-       result = simple_calculator.multiply(6,7)
+       result = SimpleCalculator.multiply(6,7)
        self.assertEqual(result,36)
 
     def test_divide(self):
-       result = simple_calculator.divide(10,2)
+       result = SimpleCalculator.divide(10,2)
        self.assertEqual(result,5)
 
 if __name__ == "__main__":
