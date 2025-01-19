024fbafbb7097fe3c2fa9f71970070283cee2c2a
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-01-19 21:40:06 +0300

    Items were edited

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,23 @@
+import unittest
+import simple_calculator
+
+class Calculation(unittest.TestCase):
+    def __init__(self):
+        pass
+
+    def test_subtract(self):
+       result = simple_calculator.subtract(6,7)
+       self.assertEqual(result, -1)
+
+    def test_add(self):
+       result = simple_calculator.add(6,7)
+       self.assertEqual(result,13)
+
+    def test_multiply(self):
+       result = simple_calculator.multiply(6,7)
+       self.assertEqual(result,36)
+
+    def test_divide(self):
+       result = simple_calculator.divide(10,2)
+       self.assertEqual(result,5)
+
