7e9ef07a806d71d337aacfaed955d9a5207f0752
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-01-26 15:45:31 +0300

    A new project

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,13 @@
+class Calculator:
+    def __init__(self, calculation_type):
+        pass
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+    
+    calculation_type = "Arithemetic Operations"
+
+    @classmethod
+    def multiply(cls, a, b):
+        return a * b
